8603f71223e2c5073aac73315f753863494c5950
Vladimir Svoboda <ze.vlad@gmail.com>
2018-11-28 18:42:59 +0100

    [HIPPEROS] Linker scripts support for ARM ELF
    This adds a new variable `RODATA_FULL_ADDR` that is used during the
    generation of linker scripts.
    If set, it completely overrides the way the location pointer is updated
    between the *text* and *rodata* sections.
    In our case, we use it to add a blank page between the two.

---
+++ ld/emulparams/elf_hipperos.sh
@@ -0,0 +1,14 @@
+# If you change this file, please also look at files which source this one:
+# armelf_hipperos.sh armelfb_hipperos.sh
+
+unset STACK_ADDR
+
+# Add a page and then align.
+# This ensures that we have an empty page.
+RODATA_FULL_ADDR=". + ${MAXPAGESIZE};\
+ . = ALIGN (${MAXPAGESIZE})"
+
+DATA_SEGMENT_ALIGN=". + ${MAXPAGESIZE};\
+ . = ALIGN (${MAXPAGESIZE})"
+DATA_SEGMENT_END=""
+DATA_SEGMENT_RELRO_END=""
